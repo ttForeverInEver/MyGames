10ae04ae0033207f6853e4291c56cd87891202ce
Tikhomir <tikhomir2011@gmail.com>
2024-05-09 16:11:31 +0300

    добавил гаим овер и исправлена шапка

--- hell/main.cpp
+++ hell/main.cpp
@@ -90,15 +90,20 @@ public:
         Texture twrsudo;
         twlsudo.loadFromFile("image/lwsudo.png");
         twrsudo.loadFromFile("image/rwsudo.png");
+        Texture tGameOver;
+        Sprite gameOver;
+        tGameOver.loadFromFile("image/gameOver.png");
+        gameOver.setTexture(tGameOver);
         bool shot = false;
         bool side = true;
         bool eb = false;
-        int hp = 10;
+        double hp = 10;
         int enemyBulletx = 0;
         int enemyBullety = 0;
         bullet.setPosition(800, 500);
         int person = 1;
         bool lyeShapka = false;
+        bool go = false;
         while (window.isOpen()) {                      
             Event event;
             while (window.pollEvent(event)) 
@@ -108,62 +113,70 @@ public:
                     window.clear();
                     window.display();
                 }
-            if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
+            if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)) && go == false)) {
                 p.sprite.move(-5, 0);
                 p.sprite.setTexture(dop1);
                 side = false;
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
+            if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)) && go == false)) {
                 p.sprite.setTexture(dop);
                 p.sprite.move(5, 0);
                 side = true;
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) {
+            if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)) && go == false)) {
                 p.sprite.move(0, 5);
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
+            if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)) && go == false)) {
                 p.sprite.move(0, -5);
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
+            if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)) && go == false)) {
                 sudo.sprite.move(-5, 0); 
                 sudo.sprite.setTexture(tlsudo);
                 if (lyeShapka == true) {
                     sudo.sprite.setTexture(twlsudo);
                 }
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {   
+            if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)) && go == false)) {
                 sudo.sprite.move(5, 0);              
                 sudo.sprite.setTexture(trsudo);
                 if (lyeShapka == true) {
                     sudo.sprite.setTexture(twrsudo);
                 }
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) {
+            if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)) && go == false)) {
                 sudo.sprite.move(0, 5);
+                sudo.sprite.setTexture(trsudo);
+                if (lyeShapka == true) {
+                    sudo.sprite.setTexture(twlsudo);
+                }
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
+            if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)) && go == false)) {
                 sudo.sprite.move(0, -5);
+                sudo.sprite.setTexture(trsudo);
+                if (lyeShapka == true) {
+                    sudo.sprite.setTexture(twlsudo);
+                }
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Space)) && person == 1) {
+            if ((Keyboard::isKeyPressed(Keyboard::Space)) && person == 1 && go == false) {
                 shot = true;
                 bull.setPosition(p.sprite.getPosition().x, p.sprite.getPosition().y);
             }
-            if ((Keyboard::isKeyPressed(Keyboard::Space)) && person == 2) {
+            if ((Keyboard::isKeyPressed(Keyboard::Space)) && person == 2 && go == false) {
                 lyeShapka = true;
                 shapka.setPosition(sudo.sprite.getPosition().x, sudo.sprite.getPosition().y);
             }
-            if ((Keyboard::isKeyPressed(Keyboard::LShift))) {
+            if ((Keyboard::isKeyPressed(Keyboard::LShift)) && go == false) {
                 person = 2;
                 sudo.sprite.setPosition(p.sprite.getPosition().x, p.sprite.getPosition().y);
             }
-            if (hp > 0) {
+            if (hp > 0 && go == false) {
                 boss1.sprite.move(d, b);
             }
-            if (bull.getPosition().x > 1900 || bull.getPosition().x < 0) {
+            if (bull.getPosition().x > 1900 || bull.getPosition().x < 0 && go == false) {
                 shot = false;
                 bull.setPosition(2000, 2000);
             }
-            if (shot == true) {
+            if (shot == true && go == false) {
                 if (side == true) {
                     bull.move(15, 0);
                 }
@@ -171,71 +184,80 @@ public:
                     bull.move(-15, 0);
                 }
             }
-            if (boss1.sprite.getPosition().x > 1800) {
+            if (boss1.sprite.getPosition().x > 1800 && go == false) {
                 d = randomInt(-5, 0);
                 eb = true;
                 enemyBulletx = -7;
                 bullet.setPosition(boss1.sprite.getPosition().x, boss1.sprite.getPosition().y);
             }
-            if (boss1.sprite.getPosition().x < 0) {
+            if (boss1.sprite.getPosition().x < 0 && go == false) {
                 d = randomInt(0, 5);
                 eb = true;
                 enemyBulletx = 7;
                 bullet.setPosition(boss1.sprite.getPosition().x + 100, boss1.sprite.getPosition().y + 100);
             }
-            if (boss1.sprite.getPosition().y > 800) {
+            if (boss1.sprite.getPosition().y > 800 && go == false) {
                 b = randomInt(-5, 0);
                 eb = true;
                 enemyBullety = -7;
                 bullet.setPosition(boss1.sprite.getPosition().x  , boss1.sprite.getPosition().y);
             }
-            if (boss1.sprite.getPosition().y < 0) {
+            if (boss1.sprite.getPosition().y < 0 && go == false) {
                 b = randomInt(0, 5);
                 eb = true;
                 enemyBullety = 7;
                 bullet.setPosition(boss1.sprite.getPosition().x + 100, boss1.sprite.getPosition(). y + 100);
             }
-            
-            if (p.sprite.getPosition().x >= 1900) {
+            if (go == true) {
+                if (Keyboard::isKeyPressed(Keyboard::O)) {
+                    boss1.sprite.setPosition(1000, 500);
+                    hp = 10;
+                    go = false;
+                    person = 1;
+                    p.sprite.setPosition(0, 0);
+                    
+                } 
+            }
+            if (p.sprite.getPosition().x >= 1900 && go == false) {
                 p.sprite.move(-5, 0);
             }
-            if (p.sprite.getPosition().x <= 0) {
-                p.sprite.move(5, 0);
+            if (p.sprite.getPosition().x <= 0 && go == false) {
+                p.sprite.move(5, 0); 
             }
-            if (p.sprite.getPosition().y >= 1000) {
+            if (p.sprite.getPosition().y >= 1000 && go == false) {
                 p.sprite.move(0, -5);
             }
-            if (p.sprite.getPosition().y <= 0) {
+            if (p.sprite.getPosition().y <= 0 && go == false) {
                 p.sprite.move(0, 5);
             }
-            if (sudo.sprite.getPosition().x >= 1900) {
+            if (sudo.sprite.getPosition().x >= 1900 && go == false) {
                 sudo.sprite.move(-5, 0);
             }
-            if (sudo.sprite.getPosition().x <= 0) {
+            if (sudo.sprite.getPosition().x <= 0 && go == false) {
                 sudo.sprite.move(5, 0);
             }
-            if (sudo.sprite.getPosition().y >= 1000) {
+            if (sudo.sprite.getPosition().y >= 1000 && go == false) {
                 sudo.sprite.move(0, -5);
             }
-            if (sudo.sprite.getPosition().y <= 0) {
+            if (sudo.sprite.getPosition().y <= 0 && go == false) {
                 sudo.sprite.move(0, 5);
             }
-            if (bullet.getPosition().x >= 1900) {
+            if (bullet.getPosition().x >= 1900 && go == false) {
                 eb = false;
                 enemyBullety = 0;
                 enemyBulletx = 0;
             }
-            if (bullet.getPosition().x <= 0) {
+            if (bullet.getPosition().x <= 0 && go == false) {
                 eb = false;
                 enemyBullety = 0;
                 enemyBulletx = 0;
             }
-            if (bullet.getPosition().y >= 1000) {
+            if (bullet.getPosition().y >= 1000 && go == false) {
                 eb = false;
                 enemyBullety = 0;
                 enemyBulletx = 0;
             }
-            if (bullet.getPosition().y <= 0) {
+            if (bullet.getPosition().y <= 0 && go == false) {
                 eb = false;
                 enemyBullety = 0;
                 enemyBulletx = 0;
@@ -244,51 +266,54 @@ public:
                 enemyBullety = 0;
                 enemyBulletx = 0;
             }
-            if (bull.getGlobalBounds().intersects(boss1.sprite.getGlobalBounds())) {
+            if (bull.getGlobalBounds().intersects(boss1.sprite.getGlobalBounds()) && go == false) {
                 hp--;
                 shot = false;
                 bull.setPosition(2000, 2000);
             }
-            if (sudo.sprite.getGlobalBounds().intersects(boss1.sprite.getGlobalBounds())) {
-                window.close();
+            if (sudo.sprite.getGlobalBounds().intersects(boss1.sprite.getGlobalBounds()) && hp > 0 && go == false) {
+                go = true;
             }
-            if (sudo.sprite.getGlobalBounds().intersects(bullet.getGlobalBounds())) {
-                window.close();
+            if (sudo.sprite.getGlobalBounds().intersects(bullet.getGlobalBounds()) && hp > 0 && go == false) {
+                go = true;
             }
-            if (shapka.getGlobalBounds().intersects(boss1.sprite.getGlobalBounds()) && person == 2 && hp > 0) {
+            if (shapka.getGlobalBounds().intersects(boss1.sprite.getGlobalBounds()) && person == 2 && hp > 0 && lyeShapka == true && go == false) {
                 lyeShapka = false;
-                hp -= 1;               
+                hp -= 5;               
                 bull.setPosition(2000, 2000);
             }
-            if (bullet.getGlobalBounds().intersects(p.sprite.getGlobalBounds()) && person == 1) {
-                window.close();
+            if (bullet.getGlobalBounds().intersects(p.sprite.getGlobalBounds()) && person == 1 && go == false) {
+                go = true;
             }
-            if (boss1.sprite.getGlobalBounds().intersects(p.sprite.getGlobalBounds()) && person == 1) {
-                window.close();
+            if (boss1.sprite.getGlobalBounds().intersects(p.sprite.getGlobalBounds()) && person == 1 && go == false) {
+                go = true;
             }
-            if (eb = true) {
+            if (eb = true && go == false) {
                 bullet.move(enemyBulletx, enemyBullety);
             }
                 
             
             window.clear();
-            if (person == 1) {
+            if (person == 1 && go == false) {
                 window.draw(p.sprite);
             }
-            if (person == 2) {
+            if (person == 2 && go == false) {
                 window.draw(sudo.sprite);
             }
-            if (shot == true) {
+            if (shot == true && go == false) {
                 window.draw(bull);
             }         
-            if(hp > 0){
+            if(hp > 0 && go == false){
                 window.draw(boss1.sprite);
             }         
-            if (eb == true && hp > 0) {
+            if (eb == true && hp > 0 && go == false) {
                 window.draw(bullet);
            }
-            if (lyeShapka == true) {
+            if (lyeShapka == true && go == false) {
                 window.draw(shapka);
+            }
+            if (go == true) {
+                window.draw(gameOver);
             }
             window.display();
         }
